c51ca02e23a28aea5945d1cee26dab7145f1c1e0
Shankara <=>
2020-06-11 06:40:08 +0530

    Implemented bfs function in graph.js

--- src/graph.js
+++ src/graph.js
@@ -4,8 +4,35 @@
 // To => to 
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
-};
+const create_graph = function(pairs) {
+  return pairs.reduce((graph, path) => {
+    const [from, to] = path;
+    if(!graph[from]) {
+      graph[from] = [];
+    }
+    graph[from].push(to);
+    return graph;
+  }, {});
+}
+
+const bfs = function(pairs, source, target) {
+  const graph = create_graph(pairs);
+  const visited = [];
+  const queue = graph[source];
+  while(queue.length) {
+    current_element = queue.shift();
+    if(current_element == target) {
+      return true;
+    }
+    graph[current_element] && graph[current_element].forEach(value => {
+      if(!visited.includes(value) && !queue.includes(value)) {
+        queue.push(value);
+      }
+    });
+    visited.push(current_element);
+  }
+  return false;
+}
 
 module.exports = {bfs};
+
